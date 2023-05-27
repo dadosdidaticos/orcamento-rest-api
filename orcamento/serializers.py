e329bf2974127c8e02628c4c651e4f0154d38861
dadosdidaticos <marcosvcc@poli.ufrj.br>
2023-05-27 05:56:35 -0300

    include minor changes to cover frontend requirements

--- orcamento/serializers.py
+++ orcamento/serializers.py
@@ -26,24 +26,30 @@ class EmployeeStateSerializer(serializers.ModelSerializer):
     health_insurance_cost=serializers.SerializerMethodField()
     department_name=serializers.SerializerMethodField()
     company_name=serializers.SerializerMethodField()
+    company=serializers.SerializerMethodField()
+    employee_name=serializers.SerializerMethodField()
     
-    relationship_type_display = serializers.SerializerMethodField(
+    relationship_type_name = serializers.SerializerMethodField(
         read_only=True)
-    employee_type_display = serializers.SerializerMethodField(read_only=True)
+    employee_type_name = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
-        fields = ('id','employee','hiring_date','health_insurance_cost','department','department_name','company_name','scenario','job','employee_type','employee_type_display','relationship_type','relationship_type_display','base_salary','benefits','performance_award','commission','initial_month','end_month')
+        fields = ('id','employee','employee_name','hiring_date','health_insurance_cost','department','department_name','company','company_name','scenario','job','employee_type','employee_type_name','relationship_type','relationship_type_name','base_salary','benefits','performance_award','commission','initial_month','end_month')
         model = EmployeeState
     
     def get_hiring_date(self,obj):
         return obj.employee.hiring_date
     def get_health_insurance_cost(self,obj):
         return obj.employee.health_insurance_cost
-    def get_employee_type_display(self, obj):
+    def get_employee_type_name(self, obj):
         return obj.EMPLOYEE_TYPES_DICT[obj.employee_type]
-    def get_relationship_type_display(self, obj):
+    def get_relationship_type_name(self, obj):
         return obj.RELATIONSHIPS_DICT[obj.relationship_type]
     def get_department_name(self,obj):
         return obj.department.name
     def get_company_name(self,obj):
         return obj.department.company.name
+    def get_company(self,obj):
+        return obj.department.company.id
+    def get_employee_name(self,obj):
+        return obj.employee.name
